6f26c32d3507dd104a8b7326ef7f15a8d4653f60
Deepak2210-droid <deepaknkl10@gmail.com>
2021-03-07 18:47:27 +0530

    Thunk creation

--- src/redux/ActionCreator.js
+++ src/redux/ActionCreator.js
@@ -1,4 +1,6 @@
 import * as ActionTypes from './ActionTypes';
+import { DISHES } from '../shared/dishes';
+
 export const addComment = (dishId, rating, author, comment) => ({
   type: ActionTypes.ADD_COMMENT,
   payload: {
@@ -7,4 +9,21 @@ export const addComment = (dishId, rating, author, comment) => ({
     author: author,
     comment: comment,
   },
+});
+//Example for redux thunk,a function is going to return a inner function which has access to dispatch and getState methods of redux
+export const fetchDishes = () => (dispatch) => {
+  dispatch(dishesLoading(true));
+
+  setTimeout(() => {
+    dispatch(addDishes(DISHES));
+  }, 2000);
+};
+//Creating Actions
+export const dishesLoading = () => ({
+  type: ActionTypes.DISHES_LOADING,
+});
+
+export const dishesFailed = (errmsg) => ({
+  type: ActionTypes.DISHES_FAILED,
+  payload: errmsg,
 });
