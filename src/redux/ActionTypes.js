b386435a25b6637650b7c66861c65636b0feaeb4
Deepak2210-droid <deepaknkl10@gmail.com>
2021-04-20 13:45:16 +0530

    Thunk

--- src/redux/ActionTypes.js
+++ src/redux/ActionTypes.js
@@ -1,1 +1,4 @@
-export const ADD_COMMENT = 'ADD_COMMNT';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const DISHES_LOADING = 'DISHES_LOADING';//Dishes ar currently fetched from server
+export const DISHES_FAILED = 'DISHES_FAILED';//Failed to feth the dishes info from server
+export const ADD_DISHES = 'ADD_DISHES';//Adding dishes to our store
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,18 +1,23 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 export const ConfigureStore = () => {
   console.log('store is created');
   const store = createStore(
-    combineReducers({
-      dishes: Dishes,
-      comments: Comments,
-      promotions: Promotions,
-      leaders: Leaders,
-    }) //recomposing global state
+    combineReducers(
+      {
+        dishes: Dishes,
+        comments: Comments,
+        promotions: Promotions,
+        leaders: Leaders,
+      },
+      applyMiddleware(thunk, logger)
+    ) //recomposing global state
   );
 
   return store;
